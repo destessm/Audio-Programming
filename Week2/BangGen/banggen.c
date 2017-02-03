96f5645f4a7032bb107f48c78ddf30f23d697608
David <destessmargiassi@gmail.com>
2017-02-03 11:29:01 -0500

    commented banggen.c

--- Week2/BangGen/banggen.c
+++ Week2/BangGen/banggen.c
@@ -1,5 +1,14 @@
-/*
-
+/* BangGen external
+   by David Estes-Smargiassi
+   February 2, 2017
+   
+   Bang Generator. Takes in three floats of unknown range and outputs bangs if
+   certain parameters are met (not supposed to be known). Essentially acts like
+   a beat generator for random inputs. Note that it does its operations on four
+   integers, not three; it uses the three previously stored integers AND the new
+   integer from inlet. This provides some variation so that the same input
+   values will not necessarily result in the same set of outputted bangs (unless
+   previously stored numbers were also the same).
  */
 
 #include "m_pd.h"
@@ -8,6 +17,7 @@ static t_class *banggen_class;
 
 char *version = "BangGen object v1.0, by David Estes-Smargiassi\n";
 
+// Struct: contains x_obj, array of outlets, and three currently-held integers
 typedef struct t_banggen
 {
   t_object x_obj;
@@ -15,6 +25,8 @@ typedef struct t_banggen
   int a,b,c;
 } t_banggen;
 
+// Constructor
+// Initializes inlets and outlets
 void *banggen_new(void)
 {
   poststring(version);
@@ -33,6 +45,7 @@ void *banggen_new(void)
   return (void *)x;
 }
 
+// General function; takes in 4 integers and outputs bangs to various outlets
 void triggerbangs(t_banggen *x, int a, int b, int c, int d)
 {
   // Outlet 0:
@@ -57,6 +70,7 @@ void triggerbangs(t_banggen *x, int a, int b, int c, int d)
     } 
 }
 
+/* Functions for Float Inputs (from each inlet) */
 void banggen_float_a(t_banggen *x, t_floatarg f)
 {
   int d = (int)f;  
@@ -78,6 +92,7 @@ void banggen_float_c(t_banggen *x, t_floatarg f)
   x->c = d;
 }
 
+// Setup
 void banggen_setup(void)
 {
   banggen_class = class_new(gensym("banggen"),
@@ -85,8 +100,15 @@ void banggen_setup(void)
 			    sizeof(t_banggen),
 			    CLASS_DEFAULT, 0);
 
+  // Inlet Methods
+
+  // first inlet (a)
   class_addfloat(banggen_class, banggen_float_a);
+  
+  // second inlet (b)
   class_addmethod(banggen_class, (t_method)banggen_float_b, gensym("in_b"), A_FLOAT, 0);
+  
+  // third inlet (c)
   class_addmethod(banggen_class, (t_method)banggen_float_c, gensym("in_c"), A_FLOAT, 0);
 }
 
